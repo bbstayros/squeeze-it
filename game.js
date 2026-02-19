01e74d541354f2e009a89d0a49ed5d59c2ca693f
bbstayros <bbstayros@gmail.com>
2026-02-19 17:11:50 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
@@ -906,4 +907,5 @@ updateXPUI();
 loadThemeUnlocks();
 loadEquippedTheme();
 resize();
+  });
 draw();
